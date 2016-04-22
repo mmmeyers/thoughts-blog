06b4c7496f2566f8acc2ca29664ead1fbcd8965b
mmmeyers <marsi286@gmail.com>
2016-04-22 14:52:51 -0400

    Devise configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :tags, :through => :post_tags
+  has_many :post_tags
+  has_many :comments
+  has_many :commentors, :through => :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :role
+  has_many :posts
+  has_many :comments
+  has_many :authored_posts, :class_name => "Post", :foreign_key => :author_id
+  has_many :commented_posts, :through => :comments, :source => :post
+  validates_presence_of :name
+  before_save :assign_role
+
+  def assign_role
+    self.role = Role.find_by name: "Regular" if self.role.nil?
+  end
+  
 end
