8086c52eb42894df3d2958da325bd673b8aa6ca0
mmmeyers <marsi286@gmail.com>
2016-05-04 19:07:16 -0400

    Made some Devise/Omniauth edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :timeoutable, 
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   belongs_to :role
   belongs_to :affiliation
@@ -24,13 +24,13 @@ class User < ActiveRecord::Base
   end
 
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 
   def assign_affiliation
     self.affiliation = Affiliation.find_by name: "None" if self.affiliation.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   scope "/admin" do 
     resources :users
   end
 
-  authenticated :user do
-    root to: 'posts#index', as: :authenticated_root
-  end
-  root to: 'welcome#index'
+  # authenticated :user do
+  #   root to: 'posts#index', as: :authenticated_root
+  # end
+
+  # root to: 'welcome#index'
+
+  root to: 'posts#index'
   
   resources :posts do
     resources :comments
